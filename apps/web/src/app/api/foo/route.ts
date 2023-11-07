9334b8996628df4ce54b902e4db44f98bbc29971
Svein Are Danielsen <sveindani@gmail.com>
2023-11-07 19:53:17 +0100

    Add product-disc suggestions to GET request in foo route

--- apps/web/src/app/api/foo/route.ts
+++ apps/web/src/app/api/foo/route.ts
@@ -1,6 +1,55 @@
-import { getRetailers, updateRetailer } from "@/lib/server";
+import {
+  getDiscs,
+  getProducts,
+  getRetailers,
+  updateManyProducts,
+  updateRetailer,
+} from "@/lib/server";
 import { NextResponse } from "next/server";
+import { getProductDiscSuggestions } from "suggestor";
 
 export async function GET(req: Request) {
-  return NextResponse.json({});
+  const [products, discs] = await Promise.all([
+    getProducts({
+      where: {
+        category: {
+          equals: "NOT_SET",
+        },
+        AND: [
+          {
+            name: {
+              contains: "grip eq",
+              mode: "insensitive",
+            },
+          },
+        ],
+      },
+    }),
+    getDiscs(),
+  ]);
+
+  const slugs = discs.map((disc) => disc.slug);
+
+  const result = products.map((product) => {
+    return {
+      product,
+      suggestions: getProductDiscSuggestions(product.name, slugs),
+    };
+  });
+
+  const update = false;
+  if (update) {
+    await updateManyProducts({
+      where: {
+        id: {
+          in: products.map((p) => p.id),
+        },
+      },
+      data: {
+        category: "OTHER",
+      },
+    });
+  }
+
+  return NextResponse.json({ result });
 }
